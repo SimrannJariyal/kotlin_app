1dc949e20ca9999b0084d50d9170efcb3f992a47
SimrannJariyal <157213118+SimrannJariyal@users.noreply.github.com>
2024-10-09 08:27:50 +0530

    login page

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,26 +1,11 @@
 package com.example.myapplication
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var navigateButton: Button
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
-
-        // Initialize the button
-        navigateButton = findViewById(R.id.button_save)
-
-        // Set the click listener
-        navigateButton.setOnClickListener {
-            // Create an Intent to start FirstActivity
-            val intent = Intent(this, FirstActivity::class.java)
-            startActivity(intent)
-        }
+        setContentView(R.layout.main_activity) // Ensure this layout exists
     }
 }
